08478e7906a113b5e73a64521079fe67e0af236a
Marcos Candeia <marrcooos@gmail.com>
2023-08-22 12:34:47 -0300

    Add deploy button
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- sections/FileTree.tsx
+++ sections/FileTree.tsx
@@ -2,15 +2,41 @@ import { SectionProps } from "deco/mod.ts";
 import { PlayContext } from "../functions/context.ts";
 import { AppContext } from "../apps/site.ts";
 
-export default function FileTree({ files }: SectionProps<typeof loader>) {
+export default function FileTree(
+  { files, context }: SectionProps<typeof loader>,
+) {
   return (
     <>
       <span>Files</span>
+      <form
+        action={`/live/invoke/play/actions/useTemplate.ts`}
+        method="GET"
+      >
+        <input type="hidden" name="playId" value={context.id} />
+        <button type="submit">Use template</button>
+      </form>
+      <form
+        action={`/live/invoke/play/actions/deploy.ts`}
+        method="GET"
+      >
+        <input type="hidden" name="playId" value={context.id} />
+        <button type="submit">Deploy</button>
+      </form>
       {files.map((file) => {
         return (
-          <div>
-            <span>{file.location.join("/")}</span>
-            <textarea>{file.content}</textarea>
+          <div class="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800 h-full w-full">
+            <label for="editor" class="sr-only">
+              {file.location.join("/")}
+            </label>
+            <textarea
+              id="editor"
+              rows={8}
+              class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              placeholder="Write an article..."
+              required
+            >
+              {file.content}
+            </textarea>
           </div>
         );
       })}
