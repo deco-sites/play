7173ad15b9a4b5f0fe2babb36f0c410d6f63932d
Marcos Candeia <marrcooos@gmail.com>
2024-02-05 15:45:29 -0300

    Set enable admin flag
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- provider/options.ts
+++ provider/options.ts
@@ -42,6 +42,7 @@ export default function provider(
       release: fromValue({
         inner: {
           decohub: {
+            enableAdmin: true,
             apps: [
               {
                 name: siteName,
