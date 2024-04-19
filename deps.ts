d5493b6bd30436641946b4f4898bf3cb24e4a1db
Marcos Candeia <marrcooos@gmail.com>
2024-04-19 08:50:48 -0300

    Fix imports from realtime
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- deps.ts
+++ deps.ts
@@ -1,4 +1,6 @@
-export { Realtime } from "../realtime/src/realtime.ts";
-export type { RealtimeState } from "../realtime/src/realtime.ts";
-export type { Env } from "../realtime/src/index.ts";
-export { FSError } from "../realtime/src/realtime.ts";
+export type { Env } from "https://denopkg.com/deco-cx/realtime@b299319cd6b13b4077046841331f35609dc3ebd5/src/index.ts";
+export {
+  FSError,
+  Realtime,
+} from "https://denopkg.com/deco-cx/realtime@b299319cd6b13b4077046841331f35609dc3ebd5/src/realtime.ts";
+export type { RealtimeState } from "https://denopkg.com/deco-cx/realtime@b299319cd6b13b4077046841331f35609dc3ebd5/src/realtime.ts";
