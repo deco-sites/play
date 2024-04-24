65664065201ed9f71f1f3257d45082a40f3c9748
Marcos Candeia <marrcooos@gmail.com>
2024-04-24 11:41:43 -0300

    Add logs
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
 import { Hypervisor } from "./hypervisor.ts";
+import * as colors from "std/fmt/colors.ts";
 import "deco/plugins/deco.ts";
+import { formatLog } from "deco/utils/log.ts";
 
 const hypervisor = new Hypervisor();
 
 const appPort = Deno.env.get("APP_PORT");
 Deno.serve(
   { port: appPort ? +appPort : 8000 },
-  hypervisor.fetch.bind(hypervisor),
+  (req) => {
+    let response: Promise<Response> | null = null;
+    const begin = performance.now();
+    try {
+      return response = hypervisor.fetch(req);
+    } finally {
+      response?.then((resp) => {
+        console.log(
+          colors.bgBlue(`[x]`),
+          formatLog({ begin, status: resp.status, url: new URL(req.url) }),
+        );
+      });
+    }
+  },
 );
